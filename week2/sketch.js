4e47cd5e3906fc307cc471dc841e5a3400c63dfc
Xin Li <230238610+xl6294@users.noreply.github.com>
2025-09-16 20:25:55 -0400

    experiments with p5.js circle functions

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,10 +1,14 @@
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
+function setup() { // runs once at the start
+  createCanvas(windowWidth, windowHeight); 
+  // createCanvas() is a function that creates a canvas for our p5.js sketch …
+
 }
 
-function draw() {
-  
+function draw() { // runs in a loop after setup
+  circle(50, 50, 500);
+  circle(50, 50, 50);
+  circle(500, 50, 50);
+  circle(50, 500, 50);
 }
