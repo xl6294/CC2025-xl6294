d2da8b57c2cffe542c86281fb48670e5ee0bb7dc
Xin Li <230238610+xl6294@users.noreply.github.com>
2025-10-15 05:20:00 -0400

    Updates

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -1,3 +1,10 @@
+let s;
+let lerpMrAmount = 0;
+let speed = 0.01;
+let targetMr = 0;
+// AstralBody
+let ms;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   s = min(width, height) / 100;
@@ -10,6 +17,19 @@ function setup() {
 
   rectMode(CORNERS); // Change the rect() mode to rect(x1, y1, x2, y2)
 
+  let now = new Date();
+  ms = now.getMilliseconds();
+}
+
+function draw() {
+  background("rgb(165,165,165)");
+
+  scale(s);
+
+  // Zoom
+  scale(1 / 0.44);
+  translate(-28, -8);
+
   push();
   translate(50, 50);
 
@@ -19,27 +39,37 @@ function setup() {
   arc(0, 0, 100, 100, PI, 0); // Largest circle
   pop();
 
-  rotate((2 * PI) / month());
+  // rotate the month text circle
+  // Negative angles rotate counterclockwise
+  targetMr = -((month() - 1) * 2 * PI) / 12;
+  lerpMrAmount = lerpMrAmount + speed;
+  lerpMrAmount = constrain(lerpMrAmount, 0, 1);
+  rotate(lerp(0, targetMr, lerpMrAmount));
 
   circle(0, 0, 100); // Largest circle
 
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 12; i++) {
     push();
     // translate(50, 30);
-    rotate((i * 2 * PI) / 4);
+    rotate((i * 2 * PI) / 12);
 
-    arc(0, 0, 100, 20, -PI / 2, PI / 2, PIE); // arc(x, y, w, h, start, stop, [mode], [detail])
+    if (i + 1 === month()) {
+      // When true.
+      fill("red");
+    } else {
+      // When false.
+      noFill();
+    }
+
+    textFont("Courier New");
+    textSize(3);
+    textAlign(CENTER);
+    text(monthText(i), 0, -15);
 
     pop();
   }
 
   pop();
-}
-
-function draw() {
-  // background("rgb(165,165,165)");
-
-  scale(s);
 
   circle(50, 30, 40); // circular window hole
 
@@ -61,10 +91,10 @@ function draw() {
   }
 
   let t = map(second() + 1, 1, 60, PI / 30, 2 * PI);
-  // t= map(millis() + 1, 1, 60, PI / 30, 2 * PI);
 
-  // let r = map(t, PI / 30, 2 * PI, 4 * PI / 3, 5 * PI / 3);
-  let r = map(abs(second() - 30), 0, 30, (5.25 * PI) / 3, (6.75 * PI) / 3);
+  lerpSec = second() + (((millis() + ms) / 1000) % 1);
+
+  let r2 = map(abs(lerpSec - 30), 0, 30, (5.25 * PI) / 3, (6.75 * PI) / 3);
 
   // draw the circle that stands for sun or moon (red or blue)
   // will travel along the path twice per minute
@@ -73,14 +103,38 @@ function draw() {
   push();
   if (second() < 30) {
     fill("red");
-    r = 2 * PI - r;
+    r2 = 2 * PI - r2;
   } else {
     fill("blue");
   }
-  circle(50 + 30 * sin(r), abs(-55 + 40 * cos(r)), 3);
+
+  circle(50 + 30 * sin(r2), abs(-55 + 40 * cos(r2)), 3);
+
   pop();
 
   // draw the circle that travels around the frame
   // to symbolize the progression of a day
   circle(50 + 20 * sin(t), abs(-30 + 20 * cos(t)), 3);
+}
+
+function monthText(m) {
+  // Create an array of month abbreviations
+  // https://web.library.yale.edu/cataloging/months
+  let monthAbbrs = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
+
+  // Subtract 1 since array indices start at 0
+  return monthAbbrs[m];
 }
---
+++ week6/sketch.js
@@ -0,0 +1,10 @@
+
+
+function setup() { // runs once at the start
+  createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() { // runs in a loop after setup
+  
+}
