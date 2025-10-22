63ef4814ef3201f28640a5b52ddebc39110a1b00
Xin Li <230238610+xl6294@users.noreply.github.com>
2025-10-22 15:16:36 -0400

    week8

--- week8a_random/sketch.js
+++ week8a_random/sketch.js
@@ -1,5 +1,6 @@
 let noisePosition = 0;
 let noiseSpeed = 0.01;
+let startingPoint = 0;
 
 function setup() {
   // runs once at the start
@@ -9,10 +10,18 @@ function setup() {
 function draw() {
   // runs in a loop after setup
   background("beige");
-  translate(width / 2, height / 2);
-  circleD = noise(noisePosition) * width;
-  circle(0, 0, circleD);
-  noisePosition = noisePosition + noiseSpeed;
+  // translate(width / 2, height / 2);
+  // circleD = noise(noisePosition) * width;
+  // circle(0, 0, circleD);
+  // noisePosition = noisePosition + noiseSpeed;
+
+  noisePosition = startingPoint;
+  for (let i = 0; i < width; i = i + 0.01) {
+    let y = noise(noisePosition) * height;
+    noisePosition = noisePosition + noiseSpeed;
+    circle(i, y, 5);
+  }
+  startingPoint = startingPoint + noiseSpeed;
 }
 
 function mousePressed() {
