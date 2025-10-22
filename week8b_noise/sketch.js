54f4caf6a5602c1f4d7178f8062e1df0ded6e155
Xin Li <230238610+xl6294@users.noreply.github.com>
2025-10-22 15:23:36 -0400

    week8 update

--- week8b_noise/sketch.js
+++ week8b_noise/sketch.js
@@ -1,65 +1,30 @@
-let points = [];
-let lineStart = 0;
-let lineEnd = 0;
-
-// Object Literal
-let otherPoints = [
-  { x: 0, y: 0 }, // center
-  { x: 0, y: -100 },
-  { x: 85, y: 50 },
-  { x: -85, y: 50 },
-  { x: 0, y: 100 },
-  { x: -85, y: -50 },
-  { x: 85, y: -50 },
-];
+let noisePosition = 0;
+let noiseSpeed = 0.01;
+let startingPoint = 0;
+let noiseTheta = 0;
+let noiseRadius = 0;
 
 function setup() {
   // runs once at the start
   createCanvas(windowWidth, windowHeight);
-
-  points = [
-    createVector(0, 0), // center
-    createVector(0, -100),
-    createVector(85, 50),
-    createVector(-85, 50),
-    createVector(0, 100),
-    createVector(-85, -50),
-    createVector(85, -50),
-  ];
 }
 
 function draw() {
-  noLoop();
-  // runs in a loop after setup
-  background(0);
+  background("lightgreen");
   translate(width / 2, height / 2);
-  stroke(255);
+
   strokeWeight(5);
+  point(0, 0);
 
-  for (let i = 0; i < points.length; i++) {
-    point(points[i].x, points[i].y);
-  }
+  noiseTheta = map(noise(noisePosition), 0, 1, 0, 2 * PI);
+  noiseRadius = map(noise(noisePosition), 0, 1, 0, width / 2);
 
-  stroke("red");
-  line(
-    points[lineStart].x,
-    points[lineStart].y,
-    points[lineEnd].x,
-    points[lineEnd].y
-  );
+  let x = cos(noiseTheta) * noiseRadius;
+  let y = sin(noiseTheta) * noiseRadius;
 
-  // console.log(random(1, 9));
-  // console.log(floor(random(1, 9)));
+  circle(x, y, 10);
 
-  let lineAmount = floor(random(1, 9));
-  for (let i = 0; i < lineAmount; i++) {
-    let start = floor(random(points.length));
-    let end = floor(random(points.length));
-    line(points[start].x, points[start].y, points[end].x, points[end].y);
-  }
+  noisePosition += noiseSpeed;
 }
 
-function mousePressed() {
-  lineStart = floor(random(points.length));
-  lineEnd = floor(random(points.length));
-}
+function mousePressed() {}
