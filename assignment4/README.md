1cca46cad3fda5534d72eb1750b050ad37ffac16
Xin Li <230238610+xl6294@users.noreply.github.com>
2025-10-23 04:07:35 -0400

    Update.

--- assignment4/README.md
+++ assignment4/README.md
@@ -4,6 +4,8 @@
 
 ///
 
+<img src="https://static.wikia.nocookie.net/nintendo/images/3/3e/MiiMode.PNG/revision/latest?cb=20191102010242&path-prefix=en" alt="drawing" width="48.5%"/> <img src="https://cdn.myportfolio.com/2dadc82b-4358-4c7c-9b93-22fae2f271af/eb619527-9c5e-47d1-9101-407629f6b1f3_carw_7x5x1920.png?h=2bbace4f37cd1c17daccf45b2b74f78e" alt="drawing" width="50%"/>
+
 ### Features
 
 ///
@@ -12,15 +14,17 @@
 
 1. [Assignment 4 Body Types I](https://editor.p5js.org/xl6294/sketches/HdV97rZJVu)
    - ///
-2. [Assignment 4 Body Types III](https://editor.p5js.org/xl6294/sketches/s9kVqHwjq)
+2. [Assignment 4 Body Types II](https://editor.p5js.org/xl6294/sketches/a8osCt1Wt)
    - ///
-3. [Assignment 4 Body Types IV](https://editor.p5js.org/xl6294/sketches/8o4zMLqtb)
+3. [Assignment 4 Body Types III](https://editor.p5js.org/xl6294/sketches/s9kVqHwjq)
    - ///
-4. [Assignment 4 Expressions](https://editor.p5js.org/xl6294/sketches/9V6QzKNGv)
+4. [Assignment 4 Body Types IV](https://editor.p5js.org/xl6294/sketches/8o4zMLqtb)
    - ///
-5. [Assignment 4 I](https://editor.p5js.org/xl6294/sketches/8F3_UrZuK)
+5. [Assignment 4 Expressions](https://editor.p5js.org/xl6294/sketches/9V6QzKNGv)
    - ///
-6. [Assignment 4 II](https://editor.p5js.org/xl6294/sketches/nEdQcDapO)
+6. [Assignment 4 I](https://editor.p5js.org/xl6294/sketches/8F3_UrZuK)
    - ///
-7. [Assignment 4 III](https://editor.p5js.org/xl6294/sketches/UXtiHbbg4)
+7. [Assignment 4 II](https://editor.p5js.org/xl6294/sketches/nEdQcDapO)
+   - ///
+8. [Assignment 4 III](https://editor.p5js.org/xl6294/sketches/UXtiHbbg4)
    - ///
--- assignment4/sketch.js
+++ assignment4/sketch.js
@@ -4,14 +4,10 @@ let robots = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
   s = min(width, height) / 100;
   scale(s);
-
-  strokeWeight(1 / s); // default stroke weight to 1 px
-
+  strokeWeight(1 / s);
   noFill();
-
   rectMode(CORNERS);
 }
 
@@ -19,6 +15,11 @@ function draw() {
   background("#f2dbfeff");
 
   scale(s);
+
+  for (let i = 0; i < robots.length; i++) {
+    robots[i].move();
+    robots[i].getMidY();
+  }
 
   // I want to illustrate the bots in a way that
   // the smaller the y coord they have, the earlier they are drawn
@@ -29,17 +30,18 @@ function draw() {
   // everytime before drawing
   // I somehow find a line of code from the following webpage and make it work
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#sorting_array_of_objects
-  robots.sort((a, b) => a.y - b.y);
+  robots.sort((a, b) => a.midY - b.midY);
 
   // drawing the shadows below each bot first
   for (let i = 0; i < robots.length; i++) {
-    robots[i].move();
+    // robots[i].move();
     robots[i].drawShadow();
     // robots[i].display();
   }
+
   // drawing the bodies and then the expressions for each bot here
   for (let i = 0; i < robots.length; i++) {
-    robots[i].move();
+    // robots[i].move();
     // robots[i].drawShadow();
     robots[i].display();
   }
@@ -48,7 +50,8 @@ function draw() {
 function mousePressed() {
   let amIHovering = false;
 
-  for (let i = 0; i < robots.length; i++) {
+  //https://www.w3docs.com/snippets/javascript/how-to-loop-through-array-and-remove-items-without-breaking-the-for-loop.html?utm_source=chatgpt.com
+  for (let i = robots.length - 1; i >= 0; i--) {
     if (robots[i].hovering == true) {
       robots.splice(i, 1);
       amIHovering = true;
@@ -56,14 +59,10 @@ function mousePressed() {
   }
 
   if (amIHovering == false) {
-    // let types = ["a", "b"];
     let randomType = random(["a", "b", "c", "d"]);
-    // let faces = ["left", "right"];
     let randomFacing = random(["left", "right"]);
-    let randomEyes = random([1, 2, 3]);
-    let randomMouth = random([1, 2]);
-
-    let r = random([0, 1, 2, 3]);
+    let randomEyes = random(["points", "dashes", "lashes"]);
+    let randomMouth = random(["smile", "neutral"]);
 
     // when coloring the bots, I want each skin color
     // to have a corresponding accent color
@@ -88,6 +87,8 @@ function mousePressed() {
       },
     ];
 
+    let r = floor(random(colorPairs.length));
+
     let tempRobot = new Robot(
       mouseX,
       mouseY,
@@ -106,15 +107,33 @@ class Robot {
   constructor(x, y, facing, render, shade, type, eyes, mouth) {
     this.x = x;
     this.y = y;
-    this.hovering = false;
+
     this.facing = facing;
+
     this.render = render;
     this.shade = shade;
-    this.shadow = "#4F136C";
+
     this.type = type;
-    this.speed = 0.5;
+
     this.eyes = eyes;
     this.mouth = mouth;
+
+    this.shadow = "#4F136C";
+    this.speed = 0.5;
+    this.midY;
+    this.hovering = false;
+  }
+
+  getMidY() {
+    if (this.type === "a") {
+      this.midY = this.y + 2.5 * s;
+    } else if (this.type === "b") {
+      this.midY = this.y - 0.25 * s;
+    } else if (this.type === "c") {
+      this.midY = this.y;
+    } else if (this.type === "d") {
+      this.midY = this.y;
+    }
   }
 
   move() {
@@ -140,6 +159,17 @@ class Robot {
       push();
 
       translate(0, 75);
+
+      noStroke();
+
+      fill(this.shadow);
+      ellipse(0, 0, 100, 30);
+
+      pop();
+    } else if (this.type == "b") {
+      push();
+
+      translate(0, 45);
 
       noStroke();
 
@@ -259,17 +289,17 @@ class Robot {
 
     // eyes
 
-    if (this.eyes === 1) {
+    if (this.eyes === "points") {
       // ver 1
       strokeWeight(5);
       point(-7, 0);
       point(7, 0);
-    } else if (this.eyes === 2) {
+    } else if (this.eyes === "dashes") {
       // ver 2
       strokeWeight(3);
       line(-10, 0, -7, 0);
       line(7, 0, 10, 0);
-    } else if (this.eyes === 3) {
+    } else if (this.eyes === "lashes") {
       // ver 3
       strokeWeight(5);
       point(-7, 0);
@@ -281,12 +311,12 @@ class Robot {
 
     // mouth
 
-    if (this.mouth === 1) {
+    if (this.mouth === "smile") {
       // ver 1
       strokeWeight(3);
       noFill();
       arc(0, 0, 40, 40, PI / 3, (2 * PI) / 3, OPEN);
-    } else if (this.mouth === 2) {
+    } else if (this.mouth === "neutral") {
       // ver 2
       strokeWeight(3);
       noFill();
@@ -314,7 +344,7 @@ class Robot {
   display() {
     // hovering over a bot will change its shadow color
     // (the change is set to not very drastic right now)
-    if (dist(mouseX, mouseY, this.x, this.y) < 50) {
+    if (dist(mouseX, mouseY, this.x, this.midY) < 7 * s) {
       this.hovering = true;
       this.shadow = "#8C28BD";
     } else {
@@ -328,5 +358,15 @@ class Robot {
     this.drawBody();
     this.drawExpression();
     pop();
+
+    // visualize the anchor points for testing
+    // https://p5js.org/reference/p5/if/#:~:text=if%20(keyIsPressed%20%3D%3D%3D%20true%20%26%26%20key%20%3D%3D%3D%20%27p%27)%20%7B%0A%20%20text(%27You%20pressed%20the%20%22p%22%20key!%27%2C%2050%2C%2050)%3B%0A%7D
+    if (keyIsPressed === true && key === "a") {
+      push();
+      stroke("white");
+      strokeWeight(2);
+      point(this.x / s, this.midY / s);
+      pop();
+    }
   }
 }
