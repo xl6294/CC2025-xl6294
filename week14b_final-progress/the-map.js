4834175402862b67cde9b14e9d9d09959996d44d
Xin Li <230238610+xl6294@users.noreply.github.com>
2025-12-03 06:31:28 -0500

    final progress

---
+++ week14b_final-progress/the-map.js
@@ -0,0 +1,16 @@
+// for the game map, I am using 2D array / matrix
+// so I could indicate the tile with
+// 0 for floor and 1 for occupied by an object
+// https://youtu.be/W9CcEDxdnmg?si=5FYQp9cIooIBlzey
+
+let floorplan = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0],
+];
--- week14b_final-progress/var.js
+++ week14b_final-progress/var.js
@@ -6,30 +6,6 @@ let wallHeight;
 
 let tileSize = 50;
 
-// for the game map, I am using 2D array / matrix
-// so I could indicate the tile with
-// 0 for floor and 1 for occupied by an object
-// https://youtu.be/W9CcEDxdnmg?si=5FYQp9cIooIBlzey
-// let floorplan = [
-//   [0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0],
-//   [0, 0, 0, 1, 0],
-//   [0, 0, 0, 0, 0],
-// ];
-
-let floorplan = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 0, 0, 0, 0, 1, 0, 0],
-  [0, 1, 1, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 0, 0, 0, 0],
-];
-
 // let columns = 5; // x => columns
 // let rows = 5; // y => rows
 
