63460c853fe47b9a7da0083bce52aff5ee7cd888
Xin Li <230238610+xl6294@users.noreply.github.com>
2025-12-09 09:23:17 -0500

    final update

---
+++ final-project/sketch.js
@@ -0,0 +1,78 @@
+let items = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(100);
+
+  // draw the wall
+  fill("beige");
+  rect(50, 50, columns * tileSize, wH);
+  translate(50, 50 + wH);
+
+  // this updates the position of player and move around
+  helperF();
+
+  items = [];
+
+  push();
+
+  scale(1, vs);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      // draw the tiles
+      push();
+
+      if (floorplan[i][j] === 1) {
+        fill(120); // occupied shadow
+        let tempItem = new Item(
+          (j + 0.5) * tileSize,
+          (i + 0.5) * tileSize * vs,
+          "object"
+        );
+        items.push(tempItem);
+      } else {
+        fill(255); // floor
+      }
+
+      stroke(0);
+      rect(j * tileSize, i * tileSize, tileSize, tileSize);
+
+      pop();
+    }
+  }
+
+  // draw the player circle here
+  push();
+  fill(120);
+  circle(player.x, player.y, player.d);
+  // draw collision guiding points
+  strokeWeight(3);
+  stroke("red");
+  point(leftX, topY);
+  point(rightX, topY);
+  point(leftX, bottomY);
+  point(rightX, bottomY);
+  pop();
+
+  pop();
+
+  playerPosition = createVector(player.x, player.y * vs);
+  push();
+  strokeWeight(3);
+  stroke("green");
+  point(playerPosition.x, playerPosition.y);
+  pop();
+
+  let tempItem = new Item(playerPosition.x, playerPosition.y, "player");
+  items.push(tempItem);
+
+  items.sort((a, b) => a.y - b.y);
+
+  for (let i = 0; i < items.length; i++) {
+    items[i].display();
+  }
+}
---
+++ final-project/var.js
@@ -0,0 +1,47 @@
+let speed;
+let posChar;
+
+let vs = 0.8; //vertical scale factor
+let wallHeight;
+
+let tileSize = 50;
+
+// for the game map, I am using 2D array / matrix
+// so I could indicate the tile with
+// 0 for floor and 1 for occupied by an object
+// https://youtu.be/W9CcEDxdnmg?si=5FYQp9cIooIBlzey
+
+let floorplan = [
+  [1, 0, 1, 1, 1, 0, 0, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+// let columns = 5; // x => columns
+// let rows = 5; // y => rows
+
+let rows = floorplan.length; // dependent variables
+let columns = floorplan[0].length; // dependent variables
+
+let mapWidth = columns * tileSize;
+let mapHeight = rows * tileSize;
+
+// creating the player profile
+let player = {
+  x: mapWidth / 2,
+  y: mapHeight / 2,
+  xd: 0, // direction indicator // from the python game workshop
+  yd: 0,
+  speed: 3,
+  d: 30,
+};
+
+let leftX, rightX, topY, bottomY; // test helper function
+
+let wH = 100; // wall height
